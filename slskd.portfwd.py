a5ad7ddb3dd6b168381bfd17ff32285b514bbc9d
tieum <matthieu@bluegreen.sh>
2024-11-01 09:16:36 +0100

    detect namespace

--- slskd.portfwd.py
+++ slskd.portfwd.py
@@ -1,22 +1,34 @@
 from __future__ import print_function
-import time
+import os, time
 import kubernetes.client
 from kubernetes.client.rest import ApiException
 from kubernetes import client, config
 from pprint import pprint
 from kubernetes.stream import stream
 
+
+def get_current_namespace():
+    ns_path = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+    if os.path.exists(ns_path):
+        with open(ns_path) as f:
+            return f.read().strip()
+
+
 config.load_incluster_config()
 api_instance = client.CoreV1Api()
 
+namespace = get_current_namespace()
+
 # get current slskd pod name
 ret = api_instance.list_namespaced_pod(
-    "slskd", watch=False, label_selector="app.kubernetes.io/name=slskd"
+    namespace, watch=False, label_selector="app.kubernetes.io/name=slskd"
 )
 for item in ret.items:
-    print("%s" % (item.metadata.name))
+    print(
+        "found pod %s running in namespace %s"
+        % (item.metadata.name, item.metadata.namespace)
+    )
     pod_name = item.metadata.name
-
 update_port_command = [
     "/bin/sh",
     "-c",
@@ -26,7 +38,7 @@ try:
     api_response = stream(
         api_instance.connect_get_namespaced_pod_exec,
         pod_name,
-        "slskd",
+        namespace,
         command=update_port_command,
         container="slskd",
         stderr=True,
